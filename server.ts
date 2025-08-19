4b3c69e96e58d54341662877664413bfd4a1c44f
karabo <seeisakarabo2@gmail.com>
2025-08-19 23:14:58 +0200

    modularized instance

---
+++ server.ts
@@ -0,0 +1,8 @@
+import express from "express"
+const app = express()
+
+
+  export const PORT =parseInt(process.env.PORT || "5000",10);
+  app.listen(PORT,"0.0.0.0",() =>{
+  console.log("Server running on port 5000")
+  });
